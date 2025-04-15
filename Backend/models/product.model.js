a3a63ec6f3740f20a029cec96f4347e011dd07eb
Faten-SHA <fatenlahlouh@gmail.com>
2025-04-15 13:36:28 +0300

    error handling middleware

--- Backend/models/product.model.js
+++ Backend/models/product.model.js
@@ -12,7 +12,7 @@ const ProductSchema = mongoose.Schema({
     categories:{type:Array},
     concern:{type:Array},
     brand:{type:String},
-    skintype:{type:Array},
+    skinType:{type:Array},
     originalPrice:{type:Number},
     discountedPrice:{type:Number},
     inStock:{type:Boolean, default:true},
@@ -20,7 +20,7 @@ const ProductSchema = mongoose.Schema({
         {
             star: {type:Number, min:1, max:5},
             comment: {type:String},
-            name: {type:String}
+            name: {type:String},
             postedBy: {type:String}
         }
     ]
