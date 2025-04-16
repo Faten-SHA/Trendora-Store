04489e83590b755e451d0f5a05c51abe7b3bbf49
Faten-SHA <fatenlahlouh@gmail.com>
2025-04-16 18:31:52 +0300

    login register logout contollers

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,6 @@
 import express from 'express'; // Importing express using this syntax because we are using ES6 modules ,in package.json we have "type": "module"
 import cors from 'cors'; // Importing cors to handle cross-origin requests
+import { notFound, errorHandler } from './Middleware/error.middleware.js'; // Importing custom error handling middleware
 const app = express(); 
 
 // Middleware /json body parser
@@ -8,5 +9,8 @@ app.use(express.json());
 //cors
 app.use(cors()); // Enable CORS for all routes 
 
+//Error handling middleware
+app.use(notFound); // Middleware to handle 404 errors
+app.use(errorHandler); // Middleware to handle other errors
 
 export default app; 
