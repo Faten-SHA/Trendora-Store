46a8349735bd3a4f5eddca77b6cba9cceb7fc9dc
Faten-SHA <fatenlahlouh@gmail.com>
2025-04-28 12:09:05 +0300

    some updates on components Navbar - Product - Products and adding rating component in Frontend folder

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,52 +1,64 @@
-import { FaSearch, FaUser } from "react-icons/fa";
-import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
-import { Badge } from "@mui/material";
-import { Link } from "react-router-dom";
+import { FaSearch, FaUser } from 'react-icons/fa';
+import ShoppingBacketIcon from '@mui/icons-material/ShoppingBasket';
+import { useSelector } from "react-redux"
 
+import Badge from '@mui/material/Badge';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 const Navbar = () => {
+
+  const [search, setSearch] = useState("");
+  const cart = useSelector((state) => state.cart)
+  const user = useSelector((state) => state.user);
+
   return (
-    <div className="flex items-center justify-between h-[100px] shadow-md bg-white">
+    <div className="flex items-center justify-between h-[100px] shadow-md px-6">
+
       <Link to="/">
-        <div className="cursor-pointer m-2 ml-8">
-          <img
-            src={"/Trendoralogo.png"}
+        <div className="cursor-pointer m-2">
+          <img src={"/Trendoralogo.png"}
             alt="Trendora logo"
             height={90}
-            width={90}
-          />
+            width={90} />
         </div>
       </Link>
+
       <div className="flex items-center m-2">
         <input
           type="text"
-          placeholder="search for an item"
-          className="p-[15px] border-2 border-[#E82561] border-solid w-[500px] outline-none rounded-lg mr-[-30px]"
+          placeholder="search"
+          className="p-[15px] border-2 border-[#D91656] border-solid w-[500px] outline-none rounded-lg mr-[-30px]"
+          onChange={(e) => setSearch(e.target.value)}
         />
-        <FaSearch className="text-[20px] cursor-pointer" />
+
+        <Link to={`/products/${search}`}>
+          <FaSearch className="text-[20px] cursor-pointer" />
+        </Link>
+
       </div>
 
       <div className="flex items-center">
         <Link to="/cart">
           <div className="mr-[20px] cursor-pointer">
-            <Badge
-              badgeContent={4}
-              color="secondary"
-              className="cursor-pointer"
-            >
-              <ShoppingBasketIcon className="text-pink-500" />
+            <Badge badgeContent={cart.quantity} color="secondary">
+              <ShoppingBacketIcon className="text-pink-500" />
             </Badge>
           </div>
         </Link>
-        <Link to="/login">
-        <div className="flex items-center space-x-2 border border-pink-300 p-2 rounded-lg hover:bg-pink-100 cursor-pointer duration-300 m-8">
-          <FaUser className="text-[#E82561] hover:text-pink-600 transition duration-600" />
-          <span className="text-[#E82561] hover:text-pink-600 font-semibold">
-            Login
-          </span>
-        </div>
-        </Link>
 
-        <div></div>
+        <Link to="/login">
+          <div className="flex items-center cursor-pointer space-x-2 border border-pink-300 p-2 rounded-lg hover:bg-pink-100 duration-300">
+            <FaUser className="text-[#e455c5] hover:text-pink-600 transition duration-300" />
+            {!user.currentUser ? <span className="text-[#D91656] hover:text-pink-600 font-semibold">
+              Login
+            </span> :
+            
+            <Link to="/myaccount"> 
+              <span  className="text-[#D91656] hover:text-pink-600 transition duration-300 font-semibold">{user.currentUser.name}</span>
+            </Link>
+            }
+          </div>
+        </Link>
       </div>
     </div>
   );
--- Frontend/src/components/Products.jsx
+++ Frontend/src/components/Products.jsx
@@ -1,20 +1,90 @@
+import { useEffect, useState } from "react";
 import Product from "./Product";
+import PropTypes from "prop-types"
+import {userRequest} from "../requestMethods"
+import {Link} from "react-router-dom";
 
-const Products = () => {
+
+
+const Products = ({ filters, sort, query }) => {
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  console.log(products)
+
+  useEffect(() => {
+    const getProducts = async () => {
+
+      try {
+        let res;
+
+        if (query) {
+          res = await userRequest.get(`/products?search=${query}`);
+        } else {
+          res = await userRequest.get("/products");
+        }
+        setProducts(res.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getProducts();
+
+  }, [
+    query
+  ])
+
+
+  useEffect(() => {
+
+    let tempProducts = [...products];
+
+    // apply filters
+
+    if (filters) {
+      tempProducts = tempProducts.filter((item) => Object.entries(filters).every(([key, value]) => {
+        if (!value) return true;
+
+        return item[key].includes(value);
+      }))
+    }
+
+
+    //Apply sorting
+
+    if (sort === "newest") {
+      tempProducts.sort(
+        (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+      )
+    } else if (sort === "asc") {
+
+      tempProducts.sort((a, b) => a.originalPrice - b.originalPrice);
+
+    } else if (sort === "desc") {
+      tempProducts.sort((a, b) => b.originalPrice - a.originalPrice);
+    }
+
+    setFilteredProducts(tempProducts);
+  }, [products, filters, sort])
+
   return (
-    <div className="flex flex-wrap mx-[40px] mt-[20px] mb-[20px]">
-        <Product img="/bag1.png"/>
-        <Product img="/crossBagsCategory.png"/>
-        <Product img="/handbagsCetegoryCover.png"/>
-        <Product img="/totebagsCategoryCover.png"/>
-        <Product img="/crossBagsCategory.png"/>
-        <Product img="/handbagsCetegoryCover.png"/>
-        <Product img="/totebagsCategoryCover.png"/>
-        <Product img="/bag1.png"/>
-        <Product img="/totebagsCategoryCover.png"/>
-      
+    <div className="flex flex-wrap mx-[40px]">
+      {filteredProducts.map((product, index) => (
+
+        <Link to={`/product/${product._id}`}>
+          <Product product={product} />
+        </Link>
+      ))
+      }
     </div>
   );
 };
+
+Products.propTypes = {
+  cat: PropTypes.string,
+  filters: PropTypes.object,
+  sort: PropTypes.string,
+  query: PropTypes.string
+}
 
 export default Products;
